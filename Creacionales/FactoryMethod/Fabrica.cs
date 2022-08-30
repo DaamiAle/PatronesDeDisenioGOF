a08402c13805ef8902a8751b4f539fca7eb958d0
DaamiAle <daamiale.recoil@gmail.com>
2022-08-30 17:35:27 -0300

    Factory Method

---
+++ Creacionales/FactoryMethod/Fabrica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronesDeDisenioGOF.Creacionales.FactoryMethod
+{
+    /// <summary>
+    /// La Frabrica produce intancias de una clase deseada, siempre que esta clase implemente la interfaz.
+    /// En este ejemplo no se le podria pedir a la fabrica de Ferrari que cree un Lamborghini.
+    /// Solo se le prodran pedir instancias de modelos de ferrari que implementen la interfaz IFerrari.
+    /// </summary>
+    internal class FabricaFerrari
+    {
+        public IFerrari CrearFerrari(string modelo)
+        {
+            switch (modelo)
+            {
+                case "A":
+                    return new LaFerrari();
+                case "B":
+                    return new Roma();
+                case "C":
+                    return new FXX();
+                default:
+                    throw new ArgumentException("Modelo no valido");
+            }
+        }
+    }
+}
---
+++ Creacionales/FactoryMethod/IFerrari.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronesDeDisenioGOF.Creacionales.FactoryMethod
+{
+    internal interface IFerrari
+    {
+        public int AcelerarMaximaVelocidad();
+    }
+}
