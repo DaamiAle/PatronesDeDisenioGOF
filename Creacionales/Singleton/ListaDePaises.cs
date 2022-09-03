08ac47fa7f6c6cb9c540b48da3720ac927ea3e73
DaamiAle <daamiale.recoil@gmail.com>
2022-09-03 19:51:03 -0300

    Singleton

---
+++ Creacionales/Singleton/ListaDePaises.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace PatronesDeDisenioGOF.Creacionales.Singleton
+{
+    /// <summary>
+    /// Singleton que representa la lista de paises del planeta.
+    /// Solo existe 1 lista de paises en el planeta
+    /// </summary>
+    public class ListaDePaises
+    {
+        private static ListaDePaises instancia;
+        private readonly List<string> paises = new();
+
+        private ListaDePaises() { }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Instancia unica del sigleton.</returns>
+        public static ListaDePaises ObetenerListaDePaises()
+        {
+            if (instancia == null)
+            {
+                instancia = new();
+            }
+            return instancia;
+        }
+        public List<string> Paises() => paises;
+    }
+}
