2641837bccea71aa5d841e170336f9eb6c1c4e55
DaamiAle <daamiale.recoil@gmail.com>
2022-09-03 20:12:57 -0300

    Prototype

---
+++ Creacionales/Prototype/Propiedad.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronesDeDisenioGOF.Creacionales.Prototype
+{
+    public class Propiedad : ICloneable
+    {
+        private string direccion;
+        private int antiguedad;
+        private int ambientes;
+        private string dueño;
+
+        public Propiedad(string direccion, int antiguedad, int ambientes, string dueño)
+        {
+            this.direccion = direccion;
+            this.antiguedad = antiguedad;
+            this.ambientes = ambientes;
+            this.dueño = dueño;
+        }
+
+        public object Clone()
+        {
+            return new Propiedad(direccion, antiguedad, ambientes, dueño);
+        }
+    }
+}
